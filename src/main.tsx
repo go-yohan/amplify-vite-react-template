1d4e5c4da6ea2c76235ddd87770752b17078ce42
Yohan Sutjandra <yohan.sutjandra@tes-h2.com>
2024-09-29 21:57:36 -0500

    Fix deployment error

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import App from "./App.tsx";
 import "./index.css";
 import { Amplify } from "aws-amplify";
 import outputs from "../amplify_outputs.json";
-import Header from "./components/Header.tsx";
 
 Amplify.configure(outputs);
 
